a85f4019aaf6da594b99e1d603fd03ed8654fc85
Aaisha Al Rubaie <gtzx7193@gmail.com>
2026-06-03 13:12:56 +0400

    added 05-Update-Delete.sql with UPDATE

--- 05-Update-Delete.sql
+++ 05-Update-Delete.sql
@@ -67,5 +67,95 @@ WHERE LicenseNumber = 'LIC-CP002';
 
 
 
+-- PART 2: DELETE TASKS
 
 
+-- 1. Delete one cancelled flight
+-- First, confirm the cancelled flight exists
+SELECT *
+FROM Flight
+WHERE FlightNumber = 'SK108'
+  AND Status = 'Cancelled';
+
+
+-- Delete the cancelled flight
+DELETE FROM Flight
+WHERE FlightNumber = 'SK108'
+  AND Status = 'Cancelled';
+
+-- Confirm deletion
+SELECT *
+FROM Flight
+WHERE FlightNumber = 'SK108';
+
+
+
+-- 2. Delete one booking linked to a cancelled flight
+-- First, confirm the booking exists and is linked to a cancelled flight
+SELECT 
+    B.BookingID,
+    B.SeatNumber,
+    B.Class,
+    B.Price,
+    F.FlightNumber,
+    F.Status
+FROM Booking B
+INNER JOIN Flight F
+    ON B.FlightID = F.FlightID
+WHERE F.Status = 'Cancelled';
+GO
+
+-- Delete one booking linked to cancelled flight SK103
+DELETE B
+FROM Booking B
+INNER JOIN Flight F
+    ON B.FlightID = F.FlightID
+WHERE F.FlightNumber = 'SK103'
+  AND F.Status = 'Cancelled';
+
+-- Confirm deletion
+SELECT 
+    B.BookingID,
+    B.SeatNumber,
+    B.Class,
+    B.Price,
+    F.FlightNumber,
+    F.Status
+FROM Booking B
+INNER JOIN Flight F
+    ON B.FlightID = F.FlightID
+WHERE F.FlightNumber = 'SK103';
+
+
+
+-- 3. Try to delete a passenger who has existing bookings
+-- First, confirm that the passenger exists and has bookings
+SELECT 
+    P.PassengerID,
+    P.FullName,
+    P.NationalID,
+    B.BookingID,
+    B.FlightID
+FROM Passenger P
+INNER JOIN Booking B
+    ON P.PassengerID = B.PassengerID
+WHERE P.NationalID = 'AE1002';
+
+-- Try to delete the passenger
+DELETE FROM Passenger
+WHERE NationalID = 'AE1002';
+
+-- Comment:
+-- Because the foreign key from Booking to Passenger uses ON DELETE CASCADE,
+-- deleting this passenger will also delete the passenger's related bookings automatically.
+-- If ON DELETE CASCADE was not used, SQL Server would prevent the delete because related bookings exist.
+
+-- Confirm what happened after deletion
+SELECT *
+FROM Passenger
+WHERE NationalID = 'AE1002';
+
+
+SELECT *
+FROM Booking
+WHERE PassengerID = 2;
